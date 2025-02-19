da4c6106fc9a654d81d96915beed44869f899f6b
Tran Gia Than <trangiathandz2004@gmail.com>
2025-02-19 19:31:38 +0700

    Update App.js with Hello World

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Hello world!
         </p>
         <a
           className="App-link"
@@ -21,5 +22,8 @@ function App() {
     </div>
   );
 }
+
+
+
 
 export default App;
