87b8bdabc8cfacb2e58c1a478d077155a2895f71
Tran Gia Than <trangiathandz2004@gmail.com>
2025-02-19 19:42:33 +0700

    an bun bo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Hôm nay ăn bún bò!
         </p>
         <a
           className="App-link"
